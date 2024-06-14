ab3bd9f2dca5495856614855e4f8361fe555e70a
cakrawrz_ <cakradwi1@gmail.com>
2024-06-15 00:03:05 +0700

    login, camera, profile, register, login

--- app/src/main/java/com/bangkit/sibivisionproject/ui/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/sibivisionproject/ui/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.bangkit.sibivisionproject
+package com.bangkit.sibivisionproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.sibivisionproject.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
--- app/src/main/java/com/bangkit/sibivisionproject/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/bangkit/sibivisionproject/ui/SplashScreenActivity.kt
@@ -1,9 +1,10 @@
-package com.bangkit.sibivisionproject
+package com.bangkit.sibivisionproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.sibivisionproject.R
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
